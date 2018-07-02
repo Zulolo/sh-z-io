f22ba8b77af181f6005778a50bafbddfe13f701a
zulolo <dodoleon@sina.com>
2018-07-02 23:04:11 +0800

    start desktop config application

---
+++ code/config/config/config/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Zulolo
+ * Date: 7/2/2018
+ * Time: 10:09 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace config
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ code/config/config/config/sh_z_002.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Zulolo
+ * Date: 7/2/2018
+ * Time: 10:18 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net;
+using EasyModbus;
+
+namespace config
+{
+	/// <summary>
+	/// Description of sh_z_002.
+	/// </summary>
+	public class sh_z_002
+	{
+		private const int SH_Z_002_DEV_INFO_REG_ADDR = 30000;
+		private const int SH_Z_002_DEV_INFO_REG_LENTH = 8;
+//		private IPAddress device_ip;
+//		private int device_port;
+		private ModbusClient modebus_client;
+		
+//		public IPAddress ip
+//		{
+//			set {
+//				device_ip = value;
+//			}
+//			get {
+//				return device_ip;
+//			}
+//		}
+//				
+//		public int port
+//		{
+//			set {
+//				if (value > 1 && value < 65536) {
+//					device_port = value;
+//				} else {
+//					device_port = 502;
+//				}				
+//			}
+//			get {
+//				return device_port;
+//			}
+//		}
+		
+		public bool check_validation()
+		{
+			if (modebus_client.Connected == false) {
+				modebus_client.Connect();	
+			}
+			if (modebus_client.Available(2000) == false) {
+				return false;
+			}
+			int[] buf = modebus_client.ReadInputRegisters(SH_Z_002_DEV_INFO_REG_ADDR, SH_Z_002_DEV_INFO_REG_LENTH);
+			if (buf != null) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		public sh_z_002(string ipAddress, int port)
+		{
+//			this.ip = IPAddress.Parse(ipAddress);
+//			this.device_port = port;
+			modebus_client = new ModbusClient(ipAddress, port);
+		}
+	}
+}
